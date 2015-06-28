425e75442d2d3731c89f934ff74795be53422a1e
ger0nim0 <len2koo@gmail.com>
2015-06-29 00:13:20 +0300

    Changed sort function to be able to sort in nonincreasing way

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -1,18 +1,15 @@
-__author__ = 'lena'
-
-
-def sort(array):
+def sort(array, increasing=True):
     for j in range(1, len(array)):
         key = array[j]
         i = j - 1
-        while i >= 0 and array[i] > key:
+        while i >= 0 and (array[i] > key) == increasing:
             array[i + 1] = array[i]
             i -= 1
         array[i + 1] = key
 
-
-
 if __name__ == '__main__':
     array = [1, 51, 5, 9, 3, -9, 31, 844, 1, 56, -5, -64, 654]
     sort(array)
+    print(array)
+    sort(array, False)
     print(array)
